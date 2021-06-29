29d1b20bd9daea98cc002990210d40325d93017c
RehamMobarak <reham.a.mobarak@gmail.com>
2021-06-29 22:26:24 +0200

    fix: bst styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,12 @@
 
 @if (count($todos) > 0)
 @foreach ($todos as $todo)
-<div class="card m-2">
-    <h2><a href="todos/{{$todo->id}}">{{$todo->Title}}</a></h2>
-    <h3>{{$todo->Content}}</h3>
-    <span class="label label-danger">{{$todo->Due}}</span>
+<div class="card col-5  m-2 ">
+    <div class="card-body m-2">
+        <h2><a href="todos/{{$todo->id}}">{{$todo->Title}}</a></h2>
+        <h3>{{$todo->Content}}</h3>
+        <span class="badge rounded-pill bg-danger">{{$todo->Due}}</span>
+    </div>
 </div>
 @endforeach
 @endif
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 @section('content')
-<a href="/" class="btn btn-default">Go Home</a>
+<a href="/" class="btn btn-secondary mt-2">Go Home</a>
 <div class="card">
     <h1>{{$todo->Title}}</h1>
-    <div class="label label-danger">{{$todo->Due}}</div>
+    <div class="badge rounded-pill bg-danger">{{$todo->Due}}</div>
     <hr>
     <p>{{$todo->Content}}</p>
 </div>
