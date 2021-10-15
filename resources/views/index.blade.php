f903707c69ce0bdac15e446888f2789fabd07bce
RehamMobarak <reham.a.mobarak@gmail.com>
2021-10-15 04:23:37 +0200

    add: change input boxes bg on focus

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 @section('content')
 <h1>Todos</h1>
+<div class="row">
+    @if (count($todos) > 0)
+    @foreach ($todos as $todo)
 
-@if (count($todos) > 0)
-@foreach ($todos as $todo)
-<div class="card col-5  m-2 ">
-    <div class="card-body m-2">
-        <h2><a href="todos/{{$todo->id}}">{{$todo->Title}}</a></h2>
-        <p class="card-subtiltle">Last modified at {{$todo->updated_at}}</p>
-        <span class="badge rounded-pill todo-bg">{{$todo->Due}}</span>
+    <div class="col-6">
+        <div class="card  m-2 ">
+            <div class="card-body m-2">
+                <h2><a href="todos/{{$todo->id}}">{{$todo->Title}}</a></h2>
+                <p class="card-subtiltle">Last modified at {{$todo->updated_at}}</p>
+                <span class="badge rounded-pill due-bg">{{$todo->Due}}</span>
+            </div>
+        </div>
     </div>
+
+    @endforeach
+    @endif
+
 </div>
-@endforeach
-@endif
 
 @endsection
