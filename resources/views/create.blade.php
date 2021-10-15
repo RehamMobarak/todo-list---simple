0ae678b79b6001a0a9b16a52757fc5b4eaa939fa
RehamMobarak <reham.a.mobarak@gmail.com>
2021-10-15 04:23:37 +0200

    add: change input boxes bg on focus

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,15 +7,15 @@
         @csrf
         <div class="form-group">
             <label for="title">Title</label>
-            <input type="text" class="form-control" name="title" id="title" placeholder="Enter title" value="{{ old('title') }}">
+            <input type="text" class="form-control input-box" name="title" id="title" placeholder="Enter title" value="{{ old('title') }}">
         </div>
         <div class="form-group">
             <label for="content">Content</label>
-            <input type="text" class="form-control" name="content" id="content" placeholder="Enter content" value="{{ old('content') }}">
+            <input type="text" class="form-control input-box" name="content" id="content" placeholder="Enter content" value="{{ old('content') }}">
         </div>
         <div class="form-group">
             <label for="due">Due</label>
-            <input type="text" class="form-control" name="due" id="due" placeholder="Enter due" value="{{ old('due') }}">
+            <input type="text" class="form-control input-box" name="due" id="due" placeholder="Enter due" value="{{ old('due') }}">
         </div>
         <button type="submit" class="btn todo-bg m-2">Submit</button>
     </form>
