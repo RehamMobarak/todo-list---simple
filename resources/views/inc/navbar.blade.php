fdb2e93d6f452d781111bbe6dad8bd5bd2a47257
RehamMobarak <reham.a.mobarak@gmail.com>
2021-10-15 03:50:49 +0200

    add: website favicon and navbar icon

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -4,9 +4,14 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav ">
+            <li class="nav-item ml-3">
+                <a class="navbar-brand" href="#">
+                    <img src="/icon/todo_icon.png" alt="todolist" width="30px" height="45px">
+                </a>
+            </li>
             <li class="nav-item">
-                <a class="nav-link {{Request::is("/") ? 'active' : ''}}" href="/">Home</a>
+                <a class="nav-link {{Request::is(" /") ? 'active' : '' }}" href="/">Home</a>
             </li>
             <li class="nav-item {{ Request::is('/todos/create') ? 'active' : '' }}">
                 <a class="nav-link" href="/todos/create">Create</a>
