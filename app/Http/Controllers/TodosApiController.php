d770e3710b6f364c76af4c89107494ed99d6dac0
RehamMobarak <reham.a.mobarak@gmail.com>
2021-10-25 00:17:17 +0200

    add: api store new todo

--- app/Http/Controllers/TodosApiController.php
+++ app/Http/Controllers/TodosApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TodosApiController extends Controller
 {
@@ -17,5 +18,25 @@ class TodosApiController extends Controller
         $todo = Todo::find($id);
         return response()->json($todo);
     }
-    
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'title' => 'required',
+            'content' => 'required',
+            'due' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+        return ["response" => $validator->messages(), "success" => false];
+        }
+
+        $new_todo = new Todo();
+        $new_todo->title = $request->input('title');
+        $new_todo->content = $request->input('content');
+        $new_todo->due = $request->input('due');
+        $new_todo->save();
+
+        return response()->json($new_todo);
+    }
+
 }
